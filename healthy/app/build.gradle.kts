18d70b1d43f8a0393a5b287d208626847a9e6af5
chenjunnian <3112037487@qq.com>
2026-03-11 12:29:04 +0800

    添加前端组件库依赖

--- healthy/app/build.gradle.kts
+++ healthy/app/build.gradle.kts
@@ -44,4 +44,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.material)                  // Material Components
+    implementation(libs.androidx.constraintlayout) // ConstraintLayout
 }
