cc8b4da3a8d0020ee767a504e5531d98afbe6a47
chenjunnian <3112037487@qq.com>
2026-03-30 19:27:03 +0800

    前后端请求成功

--- healthy/app/build.gradle.kts
+++ healthy/app/build.gradle.kts
@@ -51,4 +51,17 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.material)                  // Material Components
     implementation(libs.androidx.constraintlayout) // ConstraintLayout
+    // Retrofit 核心
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    // JSON 解析（Gson）
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    // OkHttp 客户端
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    // OkHttp 日志拦截器（调试用）
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    // Kotlin 协程
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    // ViewModel 生命周期
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 }
--- healthy/app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ healthy/app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication
 
 import HorizontalAdapter
+import com.example.myapplication.data.service.UserViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.collections.mutableListOf
 
 class HomeFragment : Fragment() {
+    private lateinit var userViewModel: UserViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,7 +21,7 @@ class HomeFragment : Fragment() {
     ): View {
         // 1. 加载布局文件
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-
+        userViewModel = ViewModelProvider(this)[UserViewModel::class.java]
         // 2. 从当前 Fragment 的视图中查找 RecyclerView
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerViewHorizontal)
 
@@ -35,6 +39,7 @@ class HomeFragment : Fragment() {
         val adapter = HorizontalAdapter(data)
         recyclerView.adapter = adapter
 
+        userViewModel.me()
         // 6. 返回填充好的视图
         return view
     }
