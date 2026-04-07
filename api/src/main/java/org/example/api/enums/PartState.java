f4713ad91f7b128dec62c8a143a243768fd1d448
chenjunnian1 <3112037487@qq.com>
2026-04-07 19:31:06 +0800

    修正枚举类

--- api/src/main/java/org/example/api/enums/PartState.java
+++ api/src/main/java/org/example/api/enums/PartState.java
@@ -3,22 +3,23 @@ package org.example.api.enums;
 import lombok.Getter;
 
 @Getter
-public enum CurrentState {
-    DRAFT((byte) 0, "草稿"),
-    SUBMITTED((byte) 1, "已提交"),
-    PROCESSING((byte) 2, "处理中"),
-    COMPLETED((byte) 3, "已完成");
+public enum PartState {
+    FLABBY((byte) 0, "松弛赘肉"),
+    SHRIVELED((byte) 1, "干瘪"),
+    AVERAGE((byte) 2, "匀称"),
+    TONED((byte) 3, "线条紧致"),
+    DEFINED((byte) 4, "腹肌清晰");
 
     private final byte value;
     private final String description;
 
-    CurrentState(byte value, String description) {
+    PartState(byte value, String description) {
         this.value = value;
         this.description = description;
     }
 
-    public static CurrentState fromByte(byte b) {
-        for (CurrentState state : values()) {
+    public static PartState fromByte(byte b) {
+        for (PartState state : values()) {
             if (state.value == b) {
                 return state;
             }
