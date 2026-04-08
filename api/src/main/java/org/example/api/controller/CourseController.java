a116e92e1fa1a14753ec63c4387bad5c714e3559
jiang <2923174960@qq.com>
2026-04-09 00:03:17 +0800

    修正规范

--- api/src/main/java/org/example/api/controller/CourseController.java
+++ api/src/main/java/org/example/api/controller/CourseController.java
@@ -1,40 +1,24 @@
 package org.example.api.controller;
 
 import org.example.api.dto.VideoDto;
-import org.example.api.entity.Video;
-import org.example.api.repository.VideoRepository;
+import org.example.api.service.CourseService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/course")
 public class CourseController {
-    private final VideoRepository videoRepository;
+    private final CourseService courseService;
 
-    public CourseController(VideoRepository videoRepository) {
-        this.videoRepository = videoRepository;
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
     }
 
     @GetMapping("/list")
     public List<VideoDto> getVideoList() {
-        // 从数据库查出所有视频
-        List<Video> videos = (List<Video>) videoRepository.findAll();
-
-        // 转换成 DTO 返回给前端
-        return videos.stream().map(video -> {
-            VideoDto dto = new VideoDto();
-            dto.setId(video.getId().intValue());
-            dto.setTitle(video.getTitle());
-            dto.setAuthor("System");
-            dto.setCoverUrl(video.getCoverUrl());
-            dto.setVideoUrl(video.getUrl());
-            dto.setDuration(video.getDuration());
-            dto.setLevel("K1");
-            return dto;
-        }).collect(Collectors.toList());
+        return courseService.getVideoList();
     }
 }
